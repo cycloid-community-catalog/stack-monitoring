e78e80f0864dce4f5f8f41dafe3fcd6998de071e
marcoldp <marco.oldp@gmail.com>
2023-09-08 12:16:40 +0200

    vm-azure: split resource_group and location from azure_cred

--- terraform/vm-mode/azure/variables.tf
+++ terraform/vm-mode/azure/variables.tf
@@ -4,7 +4,9 @@ variable "project" {}
 variable "env" {}
 
 # azure provider variables { subscription_id, tenant_id, client_id, client_secret, subcription_id }
-variable "azure_cred" {} # { subscription_id, tenant_id, client_id, client_secret }
+variable "azure_cred" {}
+variable "resource_group_name" {}
+variable "resource_group_location" {}
 
 # cycloid credentials - passed via pipeline
 variable "keypair_public" {}

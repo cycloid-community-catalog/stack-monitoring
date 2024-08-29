4959bf4af67d4c68430ccb112eadd154d9f59fac
marcoldp <marco.oldp@gmail.com>
2024-08-29 03:09:42 +0200

    fix output terraform aws

--- terraform/aws-vm/outputs.tf
+++ terraform/aws-vm/outputs.tf
@@ -1,23 +1,23 @@
 output "machine_ip_private_address" {
-  value = module.monitoring.machine_ip_private_address
+  value = module.aws-vm.machine_ip_private_address
 }
 
 output "machine_ip_public_address" {
-  value = module.monitoring.machine_ip_public_address
+  value = module.aws-vm.machine_ip_public_address
 }
 
 output "sgs_monitoring_scraping_id" {
-  value = module.monitoring.sgs_monitoring_scraping_id
+  value = module.aws-vm.sgs_monitoring_scraping_id
 }
 
 output "prometheus_domain_name" {
-  value = module.monitoring.prometheus_domain_name
+  value = module.aws-vm.prometheus_domain_name
 }
 
 output "grafana_domain_name" {
-  value = module.monitoring.grafana_domain_name
+  value = module.aws-vm.grafana_domain_name
 }
 
 output "alertmanager_domain_name" {
-  value = module.monitoring.alertmanager_domain_name
+  value = module.aws-vm.alertmanager_domain_name
 }

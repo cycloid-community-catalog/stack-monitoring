bc04ac2edb57ffbecc2a54b80115f390d75f1417
marcoldp <marco.oldp@gmail.com>
2024-11-21 12:56:26 +0100

    fix pipeline

--- terraform/aws-vm/aws-vm/outputs.tf
+++ terraform/aws-vm/aws-vm/outputs.tf
@@ -87,7 +87,7 @@ output "nginx_cert_key" {
 # ssh key
 
 output "ssh_private_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
+  value     = tls_private_key.ssh_key.private_key_openssh
   sensitive = true
 }
 

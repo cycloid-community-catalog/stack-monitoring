3812be5ebd3e6fa7777985720e859af70e7b1aa4
marcoldp <marco.oldp@gmail.com>
2025-01-02 17:47:48 +0100

    k8s: fix issue with recievers passed as creds

--- terraform/k8s/module-kube-prometheus-stack/helm.tf
+++ terraform/k8s/module-kube-prometheus-stack/helm.tf
@@ -45,7 +45,7 @@ locals {
 ---
 alertmanager:
   config:
-    receivers: ${indent(6, yamlencode(var.alertmanager_config_receivers))}
+    receivers: ${join("\n", var.alertmanager_config_receivers)}
 
 EOL
 

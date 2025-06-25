3c67d26d7c22afa520f282c09d96de52c1ccc8e1
marcoldp <marco.oldp@gmail.com>
2025-06-25 14:50:26 +0200

    fix vm provider

--- terraform/aws-vm/aws-vm/vm_network.tf
+++ terraform/aws-vm/aws-vm/vm_network.tf
@@ -60,13 +60,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_cidr" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_sgs" {
-  count = var.use_ssm_agent ?  0 : length(var.ssh_to_allow.sg)
+  count = var.use_ssm_agent ? 0 : length(var.ssh_to_allow.sg)
 
   security_group_id = aws_security_group.vm.id
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
-  referenced_security_group_id         = var.ssh_to_allow.sg[count.index]
+  referenced_security_group_id = var.ssh_to_allow.sg[count.index]
 }
 
 # allow node exporter self scraping

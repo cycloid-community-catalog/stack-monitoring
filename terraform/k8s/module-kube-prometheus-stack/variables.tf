1f5c69b90b26087e3d95b9b73188a250f3e6c555
marcoldp <marco.oldp@gmail.com>
2025-07-07 17:15:46 +0200

    k8s:  update kube-prometheus-stack helm chart to v64

--- terraform/k8s/module-kube-prometheus-stack/variables.tf
+++ terraform/k8s/module-kube-prometheus-stack/variables.tf
@@ -9,7 +9,7 @@ variable "organization" {}
 # GENERAL VARIABLES
 #
 variable "kube_prometheus_helm_version" {
-  default = "62.0.0"
+  default = "64.0.0"
 }
 variable "namespace" {}
 variable "extra_labels" {

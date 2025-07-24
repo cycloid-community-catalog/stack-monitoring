4f6b92cfb9a1a4a323163e837b119a0f33d094ab
marcoldp <marco.oldp@gmail.com>
2025-07-24 15:41:59 +0200

    vm: fix default ami change

--- terraform/aws-vm/aws-vm/vm.tf
+++ terraform/aws-vm/aws-vm/vm.tf
@@ -2,9 +2,9 @@
 # Creates the VM used to deploy grafana, prometheus and alertmanager
 ###
 
-
 # ami to use -> debian 12
 data "aws_ami" "debian" {
+  count = var.vm_ami != "" ? 0 : 1
   most_recent = true
 
   filter {
@@ -32,7 +32,7 @@ data "aws_ami" "debian" {
 
 # vm instance
 resource "aws_instance" "vm" {
-  ami                  = var.vm_ami != "" ? var.vm_ami :data.aws_ami.debian.id
+  ami                  = var.vm_ami != "" ? var.vm_ami :data.aws_ami.debian[0].id
   iam_instance_profile = aws_iam_instance_profile.vm.name
   instance_type        = var.vm_size
   key_name             = local.name_prefix
